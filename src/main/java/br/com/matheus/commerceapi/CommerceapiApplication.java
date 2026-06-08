204703a9c4fcfab445917e15215497b1e3e83837
Matheus Medeiros <promatheusmedeiros@gmail.com>
2026-06-08 15:24:28 -0300

    initial setup

---
+++ src/main/java/br/com/matheus/commerceapi/CommerceapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheus.commerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceapiApplication.class, args);
+	}
+
+}
