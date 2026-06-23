3870d735787cf92f00cf3895c54b391a2a4f31cc
Matheus Medeiros <promatheusmedeiros@gmail.com>
2026-06-23 19:16:29 -0300

    feat: add findById in UserRepository

--- src/main/java/br/com/matheus/commerceapi/repository/UserRepository.java
+++ src/main/java/br/com/matheus/commerceapi/repository/UserRepository.java
@@ -11,6 +11,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
+    Optional<User> findById(Long id);
+
     boolean existsByEmail(String email);
 
 }
