2d28784ce83579318571bd39fd5d651ae9ab0883
Matheus Medeiros <promatheusmedeiros@gmail.com>
2026-07-15 14:00:03 -0300

    feat: add getStockByProduct in StockService and findByProduct in StockRepository

--- src/main/java/br/com/matheus/commerceapi/repository/StockRepository.java
+++ src/main/java/br/com/matheus/commerceapi/repository/StockRepository.java
@@ -1,8 +1,13 @@
 package br.com.matheus.commerceapi.repository;
 
+import br.com.matheus.commerceapi.entity.Product;
 import br.com.matheus.commerceapi.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface StockRepository extends JpaRepository<Stock, Long> {}
+public interface StockRepository extends JpaRepository<Stock, Long> {
+    Optional<Stock> findByProduct(Product product);
+}
--- src/main/java/br/com/matheus/commerceapi/service/StockService.java
+++ src/main/java/br/com/matheus/commerceapi/service/StockService.java
@@ -2,9 +2,11 @@ package br.com.matheus.commerceapi.service;
 
 import br.com.matheus.commerceapi.entity.Product;
 import br.com.matheus.commerceapi.entity.Stock;
+import br.com.matheus.commerceapi.exception.NotFoundException;
 import br.com.matheus.commerceapi.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -23,6 +25,12 @@ public class StockService {
                 .build();
 
         return stockRepository.save(stock);
+    }
+
+    @Transactional(readOnly = true)
+    public Stock getStockByProduct(Product product) {
+        return stockRepository.findByProduct(product)
+                .orElseThrow(() -> new NotFoundException("Stock Not Found"));
     }
 
 }
