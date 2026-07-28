9adb55bb4d31376d966b09b7ef5592b6e45cd4e1
Matheus Medeiros <promatheusmedeiros@gmail.com>
2026-07-28 11:52:34 -0300

    feat: add UserService

---
+++ src/main/java/br/com/matheus/commerceapi/service/UserService.java
@@ -0,0 +1,40 @@
+package br.com.matheus.commerceapi.service;
+
+import br.com.matheus.commerceapi.entity.User;
+import br.com.matheus.commerceapi.exception.EmailAlreadyExistsException;
+import br.com.matheus.commerceapi.exception.UserNotFoundException;
+import br.com.matheus.commerceapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    public User findUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(UserNotFoundException::new);
+    }
+
+    public void validateUniqueEmail(String email) {
+        if (userRepository.existsByEmail(email)) {
+            log.warn("Email already exists: {}", email);
+            throw new EmailAlreadyExistsException(email);
+        }
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
