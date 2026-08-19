21859a4d2877e023514403142e8a632ed9bd579e
Matheus Medeiros <promatheusmedeiros@gmail.com>
2026-08-19 03:59:39 -0300

    test: load context against H2 in PostgreSQL mode

--- src/test/java/br/com/matheus/commerceapi/CommerceapiApplicationTests.java
+++ src/test/java/br/com/matheus/commerceapi/CommerceapiApplicationTests.java
@@ -2,8 +2,16 @@ package br.com.matheus.commerceapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:commerceapi;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class CommerceapiApplicationTests {
 
 	@Test
